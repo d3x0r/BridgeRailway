9ca7bfac932593fc1c078a25cba7ec50ca6179cc
SirIntellegence <sirintellegence2@gmail.com>
2016-07-25 14:05:53 -0600

    Added Bridge signals and such. Made the mod more future resiliant Known Issue: crafting any bridge rail related item will crash the game

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -11,5 +11,25 @@ data:extend(
     stack_size = 100,
     straight_rail = "bridge-straight-rail",
     curved_rail = "bridge-curved-rail"
-  }
+  },
+  {
+    type = "item",
+    name = "bridge-rail-signal",
+    icon = "__BridgeRailway__/graphics/bridge-rail-signal.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "transport",
+    order = "a[train-system]-d[bridge-rail-signal]",
+    place_result = "bridge-rail-signal",
+    stack_size = 50
+  },
+  {
+    type = "item",
+    name = "bridge-rail-chain-signal",
+    icon = "__BridgeRailway__/graphics/bridge-rail-chain-signal.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "transport",
+    order = "a[train-system]-e[bridge-rail-signal-chain]",
+    place_result = "bridge-rail-chain-signal",
+    stack_size = 50
+  },
 })
